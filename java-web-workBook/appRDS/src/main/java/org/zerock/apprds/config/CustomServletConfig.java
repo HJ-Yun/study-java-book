4ba2716f2148ac689aa8062dba7680f48dbacfc7
Hyeokjin Yun <hjyun6130@gmail.com>
2026-02-22 09:52:21 +0900

    26.02.22 commit

---
+++ java-web-workBook/appRDS/src/main/java/org/zerock/apprds/config/CustomServletConfig.java
@@ -0,0 +1,25 @@
+package org.zerock.apprds.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CustomServletConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        // 기존 CSS, JS, 이미지 등 정적 파일 매핑
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/font/**")
+                .addResourceLocations("classpath:/static/font/");
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/assets/**")
+                .addResourceLocations("classpath:/static/assets/");
+    }
+}
